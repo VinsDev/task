f013a4eb28e3d28a2b7f3767e2755a688b09056c
VinsDev <vinsdev185@gmail.com>
2024-01-13 14:24:14 +0100

    sidebar z index value

--- src/App.js
+++ src/App.js
@@ -19,22 +19,20 @@ function App() {
 
   return (
     <div className="flex">
-
-
       {sidebarVisible && (
         <div className='md:hidden'>
-           <div
-          className="fixed inset-0 bg-white opacity-5 z-40"
-          onClick={closeSidebar}
-        ></div>
+          <div
+            className="fixed inset-0 bg-white opacity-5 z-40"
+            onClick={closeSidebar}
+          ></div>
         </div>
-       
+
       )}
-      
-      <div className={`md:w-[16%] h-screen text-white ${sidebarVisible ? 'translate-x-0' : '-translate-x-full'} transform transition-transform duration-300 ease-in-out fixed top-0 left-0 md:static md:translate-x-0 md:flex md:inset-0`}>
+
+      <div className={`md:w-[16%] h-screen bg-white text-white ${sidebarVisible ? 'translate-x-0' : '-translate-x-full'} transform transition-transform duration-300 ease-in-out fixed top-0 left-0 md:static md:translate-x-0 md:flex md:inset-0`} style={{ zIndex: sidebarVisible ? '50' : '0' }}>
         <Sidebar />
       </div>
-      
+
       <div className='md:flex-grow'>
         <div className='md:hidden pl-4 pt-4'>
           <FaBars onClick={toggleSidebar} size={25} color='#b0b0b0' />
